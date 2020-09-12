1f659f2d710b0e13eaee8e8c17fffdd8a40c6571
Gabriel Oshiro <gabriel.oshiro@cbyk.com.br>
2020-09-12 00:23:35 -0300

    Adicionado mongoose

--- server.js
+++ server.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/nodeapi', 
+	{ 
+		useNewUrlParser: true ,
+		useUnifiedTopology: true
+	}
+);
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
 app.get('/', (req, res) => {
-	res.send('Hello Rocketseat');
+
+	const product = Product.create({
+		title: 'React Native',
+		description: 'Build native apps with React',
+		url: 'http://github.com/facebook/react-native'
+	});
+
+	return res.send("Hello Rocketseat");
 })
-app.listen(3001, () => console.log('NodeJS is running on port 3001'));
+
+app.listen(3001, () => console.error('NodeJS is running on port 3001'));
