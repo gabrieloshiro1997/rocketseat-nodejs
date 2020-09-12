bf1f67fa2edba929cd240686e2a9239fe394dc4b
Gabriel Oshiro <gabriel.oshiro@cbyk.com.br>
2020-09-12 00:50:08 -0300

    Adicionado create product

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -7,5 +7,16 @@ module.exports = {
 		const products = await Product.find();
 
 		return res.json(products);
+	},
+
+	async create(req, res) {
+		console.log(req.body)
+		try {
+			const product = await Product.create(req.body);
+	
+			return res.json(product);
+		} catch (error) {
+			return res.json(error).status(500);
+		}
 	}
 }
--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,6 @@ const routes = express.Router();
 const ProductController = require('./controllers/ProductsController');
 
 routes.get('/products', ProductController.index);
+routes.post('/products', ProductController.create);
 
 module.exports = routes;
