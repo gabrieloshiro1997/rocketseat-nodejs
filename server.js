941062fe29ea6d20e63ee350c49a824c75f354a0
Gabriel Oshiro <gabriel.oshiro@cbyk.com.br>
2020-09-11 23:40:31 -0300

    Criado primeira rota

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3001, () => console.log('Api running'));
+app.get('/', (req, res) => {
+	res.send('Hello Rocketseat');
+})
+app.listen(3001, () => console.log('NodeJS is running on port 3001'));
