9b6346b3944a95ee613851e1abf185eb44cb5b2b
Gabriel Oshiro <gabriel.oshiro@cbyk.com.br>
2020-09-12 01:19:18 -0300

    Crud finalizado e adicionado paginacao

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -4,19 +4,55 @@ const Product = mongoose.model('Product');
 
 module.exports = {
 	async index(req, res) {
-		const products = await Product.find();
+		try {
+			const { page } = req.query; 
+			const products = await Product.paginate({}, { page, limit: 10 });
 
-		return res.json(products);
+			return res.json(products);
+		} catch (error) {
+			return res.json(error).status(500);
+		}
 	},
 
 	async create(req, res) {
-		console.log(req.body)
 		try {
 			const product = await Product.create(req.body);
-	
+
 			return res.json(product);
 		} catch (error) {
 			return res.json(error).status(500);
+		}
+	},
+
+	async show(req, res) {
+
+		try {
+			const product = await Product.findById(req.params.id);
+
+			return res.json(product)
+		} catch (error) {
+			return res.send(error).status(500);
+		}
+	},
+
+	async update(req, res) {
+		try {
+			const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: false });
+
+			return res.json(product);
+		} catch (error) {
+			return res.json(error).status(500);
+		}
+	},
+
+	async destroy(req, res) {
+		try {
+			await Product.findByIdAndDelete(req.params.id);
+
+			return res.send();
+		} catch (error) {
+			return res.json(error).status(500);
+
 		}
 	}
 }
